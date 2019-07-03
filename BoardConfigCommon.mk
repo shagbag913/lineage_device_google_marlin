60839a09c3779e64c8aba66f7a6c5db33766f61e
shagbag913 <sh4gbag913@gmail.com>
2019-07-02 20:33:12 -0400

    BoardConfigCommon: always use highest Clang version
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -4,6 +4,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := $(shell grep -v based prebuilts/clang/host/$(HOST_OS)-x86/*/AndroidVersion.txt | sort | tail -n 1 | cut -d : -f 2)
 
 BOARD_VENDORIMAGE_PARTITION_SIZE := 314572800
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
