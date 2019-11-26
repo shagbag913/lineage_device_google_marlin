LineageOS/android_device_google_marlin

marlin: squash of proprietary files commits
Author: Dan Pasanen <dan.pasanen@gmail.com>
Date:   Thu Dec 29 12:07:33 2016 -0600
    marlin: add proprietary files
    Change-Id: I152db0ae552b865accebfaecc26ccfdc7bade270

Author: Ethan Chen <intervigil@gmail.com>
Date:   Mon Feb 19 22:35:31 2018 -0800
    marlin: Don't relocate vendor/app platform apps to priv-app
    Change-Id: Ib8dda589bda95d0911768e0f1d4535fcfdeacae8

Author: razorloves <razorloves@gmail.com>
Date:   Mon Apr 16 19:52:00 2018 -0500
    marlin: Add nexus.xml sysconfig to proprietary files list
    Change-Id: I0dc860b81f706f051c34ea3220d5f3fcf8b173b7

Author: razorloves <razorloves@gmail.com>
Date:   Mon Oct 9 14:05:53 2017 +0300
    marlin: Add google camera dependencies
    Change-Id: I168c709f038711f9dced46aac813dcdec84b7307

Author: razorloves <razorloves@gmail.com>
Date:   Thu Aug 2 19:59:52 2018 -0500
    marlin: Add proprietary verizon bits for wfc/vowifi
    Change-Id: I0e235ad2c6184d26f9822f8e355c08bb5be64b66

Change-Id: I152db0ae552b865accebfaecc26ccfdc7bade270

extract-files.sh
@@ -1,0 +1,56 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=marlin
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+
+extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh

shagbag913/lineage_device_google_marlin

Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

extract-files.sh
@@ -1,0 +1,56 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=marlin
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh